63e5e19485f308ce743f4425540ec3b08004085e
Khoa1410 <vtrankhoa1410@gmail.com>
2025-08-08 12:17:21 +0700

    add install plugin file

---
+++ install-nvim-package.sh
@@ -0,0 +1,32 @@
+sudo apt update
+sudo apt install build-essential cmake
+sudo apt install ripgrep
+sudo apt install fzf
+sudo apt install nodejs npm
+sudo apt install lazygit
+sudo apt install lldb
+sudo apt install xclip xsel
+sudo apt install python3
+sudo apt install git python3 python3-pip python3-setuptools
+sudo apt install python3-pip
+sudo apt install python3-debugpy
+sudo apt install python3-pylint
+sudo apt install python3-cpplint
+sudo apt install python3-black
+sudo apt install python3-pandas
+sudo apt install cargo
+sudo apt install lua5.4
+sudo apt install luarocks
+sudo n -stable
+git clone https://github.com/microsoft/vscode-js-debug
+cd vscode-js-debug
+sudo npm install
+sudo npm run compile
+cd ..
+sudo npm install -g eslint_d
+sudo npm install -g prettier
+sudo npm install -g shfmt
+sudo cargo install stylua
+sudo cargo install viu
+
+
